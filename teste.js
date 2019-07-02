c9d99bb06a4c0579a17b4503ec6eb1208c143cf0
Gustavo <gustavolucena4089@gmail.com>
2019-07-01 23:29:21 -0300

    Adicionando e Removendo items

--- teste.js
+++ teste.js
@@ -1,7 +1,19 @@
+var ing = [];
 function Adicionar(){
-	var receive = document.getElementById("lista").innerHTML;
-	var ing = document.getElementById("campo").value;
-	console.log(ing);
-	document.getElementById("lista").innerHTML = receive+"<li>"+ing+"</li>";
-	document.getElementById("campo").value = '';
+	ing.push(document.getElementById("campo").value.toUpperCase());
+	Mostrar();
+	document.getElementById("campo").value = "";
+}
+
+function Remover(){
+	ing.pop();
+	Mostrar();
+	document.getElementById("campo").value = "";
+}
+
+function Mostrar(){
+	document.getElementById("lista").innerHTML = "<li>"+ing[0]+"</li>";
+	for(var i = 1;i < ing.length && ing.length > 0;i++){
+		document.getElementById("lista").innerHTML += "<li>"+ing[i]+"</li>";
+	}
 }
